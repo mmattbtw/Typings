ddbe72c1d85044b1bd731a0c0ec725c5b2105eec
Anatole <anatole@xiantis.net>
2021-02-27 11:47:39 +0100

    Typedef TwitchUser, BearerToken (DataStructure)

--- typings/DataStructure.d.ts
+++ typings/DataStructure.d.ts
@@ -1,9 +1,44 @@
+import { API } from './API';
+
 export namespace DataStructure {
+	/**
+	 * An Emote object, representing an emote created by the app
+	 * 
+	 * @collection emotes
+	 */
 	export interface Emote {
 		name: string;
 		submitted_by?: string;
 		private?: boolean;
 		mime?: string;
 		url: string;
+	}
+
+	/**
+	 * A TwitchUser object, obtained through an OAuth2 connection by an end user
+	 * 
+	 * @collection users
+	 */
+	export interface TwitchUser {
+		broadcaster_type: string;
+		description: string;
+		display_name: string;
+		id: string;
+		login: string;
+		offline_image_url: string;
+		profile_image_url: string;
+		type: string;
+		view_count: string;
+		email: string;
+		created_at: string;
+	}
+
+	/**
+	 * A bearer token grant object linked to a TwitchUser, obtained from a code exchange of an OAuth2 connection by an end user
+	 * 
+	 * @collection oauth
+	 */
+	export interface BearerToken extends API.OAuth2.AuthCodeGrant {
+		user_id: string;
 	}
 }
