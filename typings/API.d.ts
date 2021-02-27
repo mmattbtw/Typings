ce9dc3915d0a87f8da29f60615adb35178aafbfe
Anatole <anatole@xiantis.net>
2021-02-27 11:47:52 +0100

    Remove TwitchUser from API namespace

--- typings/API.d.ts
+++ typings/API.d.ts
@@ -5,21 +5,7 @@ export namespace API {
 			refresh_token: string;
 			expires_in: number;
 			scope: string[];
-			token_type: 'bearer'
+			token_type: 'bearer';
 		}
-	}
-
-	export interface TwitchUser {
-		broadcaster_type: string;
-		description: string;
-		display_name: string;
-		id: string;
-		login: string;
-		offline_image_url: string;
-		profile_image_url: string;
-		type: string;
-		view_count: string;
-		email: string;
-		created_at: string;
 	}
 }
