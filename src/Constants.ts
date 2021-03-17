a2bc1ab0d5ddcf87f343a14f212fe9de6a0572c2
Anatole <anatole@xiantis.net>
2021-03-17 07:06:31 +0100

    Constants - emote name regex

--- src/Constants.ts
+++ src/Constants.ts
@@ -16,5 +16,14 @@ export namespace Constants {
 			DISABLED = 2,
 			LIVE = 3
 		}
+
+		export const NAME_REGEXP = new RegExp(/^[A-Za-z_\-\(\)\:0-9]{2,100}$/);
+		export const MAX_EMOTE_LENGTH = 100;
+		export const MIN_EMOTE_LENGTH = 2;
+		export const NAME_PATTERN_ERROR = ''.concat(
+			'Emote name must be between ',
+			`${MIN_EMOTE_LENGTH}-${MAX_EMOTE_LENGTH} characters, `,
+			`be alphanumeric, and it may contain underscores and dashes.`
+		);
 	}
 }
